f347f0b747b0e9058745b0e4cf8671a50e1481f5
oper <seisenbaianuarr@gmail.com>
2024-12-09 10:53:20 +0500

    commited

---
+++ index.js
@@ -0,0 +1,45 @@
+//task 1 
+let Eshop={
+    name: "Asemai",
+    phoneNomber: "8 777 000 20 08",
+    prduks: [
+       {
+        name1: "apple",
+        baga: "350000",
+        sany: "10"
+       },
+       {
+           name1: "samsung",
+           baga: "350000",
+           sany: "10"
+       },
+       {
+           name1: "redmi",
+           baga: "350000",
+           sany: "10"
+       },
+       {
+           name1: "redmi not",
+           baga: "350000",
+           sany: "10"
+       },
+       {
+           name1: "redmi t9",
+           baga: "350000",
+           sany: "10"
+       },
+    ],
+    
+   }
+   console.log(Eshop);
+   
+   //task 2
+   let updateNameMagazin = prompt("напишите новое название магазина");
+   changeShopName(updateNameMagazin)
+   let = function changeShopName(updateName) {
+       Eshop.name=updateNameMagazin  
+   }
+   console.log(Eshop);
+   
+   //task 3
+   
